c438d3edfcb37c1f7e1220ebf3c6e5812079ba21
Abir <abirhammache@gmail.com>
2020-06-05 09:54:59 -0700

    ParameterizedTests Configuration done

---
+++ src/test/java/Detctor/CSVReadingManager/CSVReadingManagerTest.java
@@ -0,0 +1,21 @@
+package Detctor.CSVReadingManager;
+
+import org.codehaus.groovy.syntax.Numbers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CSVReadingManagerTest {
+
+    @Test
+    void readFile() {
+    }
+
+//    @ParameterizedTest
+//    @ValueSource(ints = {1, 3, 5, -3, 15, Integer.MAX_VALUE}) // six numbers
+//    void isOdd_ShouldReturnTrueForOddNumbers(int number) {
+//
+//    }
+}
